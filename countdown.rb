043e7b94225002f888222e3d8a24c3994c7a1a57
Sana Zaidi <sana.rizvi.5@gmail.com>
2019-11-15 19:00:05 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,13 @@
-#write your code here
-
+def loop 
+  new_years_countdown = 0
+  while new_years_countdown <= 10 
+  puts new_years_countdown 
+  new_years_countdown += 1
+end 
+  
 def countdown
+  "HAPPY NEW YEAR!" = 10
+  while "HAPPY NEW YEAR!" > 0 
+  puts "HAPPY NEW YEAR!"
+  "HAPPY NEW YEAR!" -+ 10 
 end
